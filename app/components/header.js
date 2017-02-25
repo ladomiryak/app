1dcaf1f0b0e540900e4b30ee6ac54fdc5011620f
ladomiryak <ladomiryak@gmail.com>
2017-02-25 11:47:29 +0200

    routing

--- app/components/header.js
+++ app/components/header.js
@@ -9,7 +9,7 @@ class Header extends React.Component{
                 <h2>Welcome to my kitchen</h2>
                 <ul className="nav nav-tabs">
                    <li className="nav-item"><Link to={'/'}>Home</Link></li>
-                    <li className="nav-item"><Link to={'/admin'}>Admin</Link></li>
+                    <li className="nav-item"><Link to={'/about'}>About</Link></li>
                     <li className="nav-item"><Link to={'/form'}>Form</Link></li>
 
                 </ul>
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import {Router, Route, Link, browserHistory} from 'react-router';
-import Home from '../components/home'
+import Home from '../components/home';
+import Header  from '../components/header';
+import About from '../components/about';
 
 
 var routes = (
+
     <Router history={browserHistory}>
+        <Header/>
         <Route path={'/'} component={Home}></Route>
+        <Route path={'/about'} component={About}></Route>
     </Router>
 );
 
