6cc68e34a8ff72da516fe1ed3821e91259596961
valeri <web2@in-web.sk>
2017-03-15 16:23:14 +0100

    login logout

--- src/reducers/admin.js
+++ src/reducers/admin.js
@@ -1,9 +1,19 @@
 export default function admin(state = 'LOCKED', action) {
-    if(action.type === 'LOG_IN' && action.payload === '12345') {
-        return 'UNLOCKED';
-    } else {
-        return 'LOCKED';
-    }
+    switch (action.type) {
+        case 'LOG_IN':
 
+            if( action.payload === '12345'){
+                return 'UNLOCKED';
+            }
+            else {
+                return 'LOCKED';
+            }
+
+        case 'LOG_OUT':
+            return 'LOCKED';
+
+        default:
+            return state;
+    }
 
 }
